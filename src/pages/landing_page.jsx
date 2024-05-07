51754d0e443d9b621a1afb6cc8b57e2caf643d05
nabeena <nabeenasinjalee@gmail.com>
2024-05-07 12:47:50 +0545

    clone commit

---
+++ src/pages/landing_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FirstSectionLanding from '../section/first_section'
+
+function LandingPage() {
+  return (
+    <div>
+      <FirstSectionLanding/>
+    </div>
+  )
+}
+
+export default LandingPage
