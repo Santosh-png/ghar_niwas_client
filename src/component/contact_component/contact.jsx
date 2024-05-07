bbf01e03584c903903710b970660b86f8631e61c
SimaThapa <sima.thapa3333@gmail.com>
2024-05-07 14:33:37 +0545

    after removing the divider

--- src/component/contact_component/contact.jsx
+++ src/component/contact_component/contact.jsx
@@ -12,7 +12,7 @@ function Contact(){
         color:'#292C6A',
         margin:'10px'
     }}>
-     <Typography >
+     <Box>
             <h3 className='responsive_fontsize24'>Quick Links</h3>
             <ul className='responsive_fontsize20'>
                 <a href="#"><li>Home</li></a>
@@ -22,9 +22,9 @@ function Contact(){
                 <a href="#"><li>Contact Us</li></a>
                 
             </ul>
-        </Typography> 
-       <div className='vertical-divider'></div>
-        <Typography >
+        </Box> 
+       {/* <div className='vertical-divider'></div> */}
+        <Box >
             <h3 className='responsive_fontsize24'>Properties</h3>
             <ul className='responsive_fontsize20'>
                 <a href="#"><li>House for Sale/Rent</li></a>
@@ -32,9 +32,9 @@ function Contact(){
                 <a href="#"><li>Apartment for Sale/Rent</li></a>
             
             </ul>
-        </Typography> 
-        <div className='vertical-divider'></div>
-        <Typography >
+        </Box> 
+        {/* <div className='vertical-divider'></div> */}
+        <Box>
             <h3 className='responsive_fontsize24'>Contact Info</h3>
             <ul className='responsive_fontsize20'>
                 <li>Kusunti 13</li>
@@ -48,7 +48,7 @@ function Contact(){
             <div>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28271.991916311705!2d85.29495851139528!3d27.655502971182738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb177f078f1cd9%3A0xb514415c5a76afb3!2sKusunti%2C%20Lalitpur%2044700!5e0!3m2!1sen!2snp!4v1715060575540!5m2!1sen!2snp" width="400" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-        </Typography> 
+        </Box> 
        
 
     </Box>
