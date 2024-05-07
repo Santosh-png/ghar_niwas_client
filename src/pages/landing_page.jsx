27dab36b6a6776c584bde71409f6386dfda2f3d2
Santosh Bista <santoshbist69@gmail.com>
2024-05-07 17:08:59 +0545

    landingpage updates

--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -1,21 +1,4 @@
-import React from 'react';
-<<<<<<< HEAD
-import AboutSection from '../pages/AboutSection';
-import ClientFeedback from '../component/clientFeedbackList';
 
-
-function LandingPage() {
-    return (
-      <>
-      <ClientFeedback/>
-      <AboutSection />
-      </>
-    );
-  }
-
-export default LandingPage;
-
-=======
 import FirstSection from '../component/cover_section/first_section'
 import NavBar from '../component/navbar_component/navbar_component'
 import Contact from '../component/contact_component/contact'
@@ -35,4 +18,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
->>>>>>> edcbf65f4990bedf911c7b32df7a98e00f693cb8
