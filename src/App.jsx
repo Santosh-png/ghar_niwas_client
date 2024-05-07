5f7cf3e1614d1f06337b08d9e333802f3004faa4
nabeena <nabeenasinjalee@gmail.com>
2024-05-07 13:46:55 +0545

    landing page and first section modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import NavBar from './component/navbar_component/navbar_component'
-import Footer from './component/footer_component/footer'
-import Contact from './component/contact_component/contact'
+import React from 'react';
+
 
 import './global.css'
 import LandingPage from './pages/landing_page'
@@ -9,11 +7,7 @@ import LandingPage from './pages/landing_page'
 function App() {
       return(
         <>
-      
-        <NavBar/>
         <LandingPage />
-        <Contact/>
-        <Footer/>
         </>
       );
      
