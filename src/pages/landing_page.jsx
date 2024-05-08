006b118ff73460c9abf826bbb5759721a6d9c930
unknown <samimamiya091@gmail.com>
2024-05-07 17:31:37 -0700

    premium ...

--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import FirstSectionLanding from '../section/first_section'
 import PremiumSection from '../component/premiumListing/premiumListingSection';
-import PremiumList from '../component/premiumListing/premiumListingService'
 function LandingPage() {
   return (
     <div>
