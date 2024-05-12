3962f4ea3429119ab0fb441cc1df80da70f05553
Santosh Bista <santoshbist69@gmail.com>
2024-05-12 15:58:16 +0545

    update feedback

--- src/component/feedback/clientFeedbackList.jsx
+++ src/component/feedback/clientFeedbackList.jsx
@@ -9,7 +9,7 @@ import ClientFeedbackItems from './ClientFeedbackItems';
 function ClientFeedbackList() {
     const settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
