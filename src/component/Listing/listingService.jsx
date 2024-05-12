3930d5f0eba908ed2a6e4c6e4e7fada3ff1459dc
Santosh Bista <santoshbist69@gmail.com>
2024-05-12 17:33:25 +0545

    updating listing section

--- src/component/Listing/listingService.jsx
+++ src/component/Listing/listingService.jsx
@@ -9,48 +9,55 @@ import ListingItems from "./ListingItems";
 function Listing(){
   const [showAllCards, setShowAllCards] = useState(false);
 
-  const settings={
-    dots:true,
-    infinite:false,
-    speed:500,
-    centerMode:true,
-    slidesToShow:3,
-    slidesToScroll:1,       
-    autoplay:true,
-    arrows:false,
-    responsive:[
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+  // const settings={
+  //   dots:true,
+  //   infinite:false,
+  //   speed:500,
+  //   centerMode:true,
+  //   slidesToShow:3,
+  //   slidesToScroll:1,       
+  //   autoplay:true,
+  //   arrows:false,
+  //   responsive:[
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
+  // };
 
   const renderCardSets = () => {
     const cardSets = [];
     for (let i = 0; i < ListingItems.length; i += 3) {
       cardSets.push(
-        <div key={i} style={{ display: 'flex', justifyContent: 'space-between', width:'100%', alignContent:"space-around" }}>
+        <div  key={i} 
+        style={{ 
+          display: 'flex', 
+          justifyContent: 'space-between', 
+          width:'100%', 
+          alignContent:"space-around" ,
+          backgroundColor: "green"
+        }}>
           {ListingItems.slice(i, i + 3).map((item) => (
             <div key={item.id} className="top-card-wrapper" style={{width:"100%", marginRight:"30px", padding:"20px"}}>
               <ListingCard
