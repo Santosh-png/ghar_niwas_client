875bb0b5a55477e21b9cca4302c1166327179472
Ashika Paudel <asikapaudel93@gmail.com>
2024-05-17 16:55:36 +0545

    background

--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -1,8 +1,9 @@
 
-import FirstSection from '../component/cover_section/first_section'
-import NavBar from '../component/navbar_component/navbar_component'
-import Contact from '../component/contact_component/contact'
-import Footer from '../component/footer_component/footer'
+import './backgroundcolor.css';
+import FirstSection from '../component/cover_section/first_section';
+import NavBar from '../component/navbar_component/navbar_component';
+import Contact from '../component/contact_component/contact';
+import Footer from '../component/footer_component/footer';
 import PremiumSection from '../component/premiumListing/premiumListingSection';
 import TopSection from '../component/topListing/topListingSection';
 import FeaturedSection from '../component/featuredListing/featuredSection';
@@ -11,23 +12,40 @@ import AboutSection from './AboutSection';
 import ClientFeedback from '../component/feedback/ClientFeedback.component';
 import ListSection from '../component/Listing/ListingSection';
 
-
 function LandingPage() {
   return (
-    <div>
-      <NavBar />    
-      <FirstSection/>
-      <ListSection />
-      <PremiumSection/>
-      <TopSection/>
-      <FeaturedSection/>
-      <UploadedSection/>
-      <ClientFeedback/>
-      <AboutSection/>
+    <div style={{ backgroundColor:  'linear-gradient(to right bottom,  #5F7FB, #BF7F5F)' }}>
+      <NavBar />
+      <FirstSection />
+      <div style={{ marginBottom:'20px', backgroundColor: 'white' }}>
+        <ListSection />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+        <PremiumSection />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+        <TopSection />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+      <FeaturedSection />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+      <UploadedSection />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+      <ClientFeedback />
+      </div>
+      <div style={{ marginBottom: '20px', backgroundColor: 'white' }}>
+      <AboutSection />
+      </div>
+      <div style={{backgroundColor: 'white' }}>
       <Contact />
+      </div>
+      <div style={{backgroundColor: 'white' }}>
       <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
 export default LandingPage;
